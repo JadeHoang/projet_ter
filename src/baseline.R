06cee8cd3058f46df66d79f8f170c75d89e9dcd2
Pierre Prablanc <pierre.prablanc.pro@gmail.com>
2019-01-20 23:20:42 +0100

    Modification baseline normalisation somme + Ajout evaluation baseline

--- src/baseline.R
+++ src/baseline.R
@@ -1,35 +1,97 @@
-#### Les vecteurs embedding ####
+# Fonctions pour le document embedding
+# Baseline: combinaison linéaire des vecteurs d'embedding des mots du document
+#         - barycentre: moyenne des vecteurs
+#         - tfidf     : pondération des vecteurs par tfidf
+#         - okapi     : pondération des vecteurs par okapi
 
-# Chargement 50000 vecteurs de mots 
+load_word_embedding_vector <- function(filename, header = FALSE , stringsAsFactors = FALSE, sep = " "  ,quote= "", nrows = 50000, skip = 1){
+  word_vec <- read.csv(filename, header = header ,stringsAsFactors = FALSE,
+                       sep = sep ,quote= "", nrows = nrows, skip = skip)
+  names(word_vec) <- NULL
+  return( list( vec = word_vec[, -1], vocabulary = word_vec[, 1]) )
+}
 
-vec_wiki_news <- read.csv("../../wiki-news-300d-1M.vec", header = FALSE,stringsAsFactors = FALSE,sep = " ",quote= "", nrows = 50000, skip = 1)
-vec_cc <- read.csv("../../cc.en.300.vec", header = FALSE,stringsAsFactors = FALSE,sep = " ",quote= "", nrows = 50000, skip = 1)
+weight_embedding <- function(corpus, ponderation, k = 2 , b = 0.75){
+  require(udpipe)
+  
+  # description: calculer les ponderation tf idf et okapi
+  # input:
+  # - corpus
+  # - ponderation: type de ponderation : tf-idf ou okapi
+  # - k et b: les paramètres de Okapi
+  # output: selon le type de ponderation, les moyennes des tf-idf ou Okapi par termes
+  x <- document_term_frequencies(x = corpus, split = " ")
+  x <- document_term_frequencies_statistics(x, k, b)
+  
+  if (ponderation == "tfidf"){
+    return(x[, c("term","tf_idf")])
+  }else if (ponderation == "okapi"){
+    return(x[, c("term","bm25")])
+  }else message("Entrez le type de ponderation.")
+}
 
-names(vec_wiki_news) <- NULL
-names(vec_cc) <- NULL
-
-#### Fonctions ####
-
-centroid <- function(mots){
+document_embedding <- function(embedding, document, methode = "barycentre", weights = NA){
   # description: calculer le barycentre des vecteurs de word embedding
-  # input: liste de mots
-  # ouput: le barycentre 
+  # input: liste de mots, vecteur de word embedding, methode = c("barycentre","tfidf","okapi")
+  # ouput: la moyenne
+  
+  word_vec <- embedding$vec
+  vocabulary <- embedding$vocabulary
+
+  #tokens
+  mots <- unlist(strsplit(document, split = " "))
   
   #trouver l'index des mots entrés dans les vecteurs des mots embedding
-  ind_mots <- match(mots,vec_wiki_news[,1])
+  ind_mots <- na.omit(match(mots , vocabulary))
+  N <- length(ind_mots) # Tous les mots du document ne figurent pas dans l'embedding
   
-  #calculer la moyenne
-  barycentre <- mapply(sum,vec_wiki_news[ind_mots,-1])/as.numeric(length(mots))
-  
-  return (barycentre)
+  if (methode == "barycentre") {
+    barycentre <- mapply(sum, word_vec[ind_mots, ]) / N
+    return (barycentre)
     
+  }else if (methode == "tfidf") {
+    
+    # words_in_vec <- as.vector(word_vec[ind_mots,1])
+    # pond <- dtm_tfidf[num_doc , match(words_in_vec[,1],colnames(dtm_tfidf))]
+    # tfidf <- colSums(pond * as.matrix(word_vec[ind_mots,-1]))/as.numeric(length(pond))
+    
+    words_in_vec <- as.vector( vocabulary[ind_mots] )
+    pond <- as.numeric( as.matrix( weights[match(words_in_vec, weights$term), "tf_idf"] ) )
+    Z <- sum(pond)
+    tfidf <- colSums(pond * as.matrix( word_vec[ind_mots, ] ) ) / Z
+    return (tfidf)
+    
+  }else if (methode == "okapi"){
+    words_in_vec <- as.vector( vocabulary[ind_mots] )
+    pond <- as.numeric(as.matrix(weights[match(words_in_vec, weights$term), "bm25"]))
+    Z <- sum(pond)
+    okapi <- colSums(pond * as.matrix(word_vec[ind_mots,-1])) / Z
+    return (okapi)
+  }
 }
 
 #### Tester ####
-# centroid()
 
-
-
+# # Chargement des données du corpus de documents
+# data.text <- readLines("../dataset/cora_modified/data.txt",encoding = "utf8")
+# 
+# tfidf_avg <- weight_embedding(corpus = data.text, ponderation = 'tfidf')
+# okapi_avg <- weight_embedding(corpus = data.text, ponderation = "okapi")
+# 
+# 
+# # load word embedding vectors
+# file_vec <- "../dataset/wiki-news-300d-1M.vec/wiki-news-300d-1M.vec"
+# word_embedding <- load_word_embedding_vector(file_vec)
+# 
+# 
+# emb_doc_bar <- document_embedding(word_embedding, doc1, methode = "barycentre")
+# emb_doc_tfidf <- document_embedding(word_embedding, doc1, methode = "tfidf", weights = tfidf_avg)
+# emb_doc_okapi <- document_embedding(word_embedding, doc1, methode = "okapi", weights = okapi_avg)
+# 
+# 
+# doc_emb_bar <- sapply( data.text, function(doc) document_embedding(word_embedding, doc, methode = "barycentre"))
+# doc_emb_tfidf <- sapply( data.text, function(doc) document_embedding(word_embedding, doc, methode = "tfidf"), weights = tfidf_avg)
+# doc_emb_okapi <- sapply( data.text, function(doc) document_embedding(word_embedding, doc, methode = "okapi"), weights = okapi_avg)
 
 
 
--- src/evaluation_draft.R
+++ src/evaluation_draft.R
@@ -17,29 +17,132 @@
     # Theory
 
 
-# Chargement des données
+#### Chargement des données
 
-data_filename <- './dataset/cora_modified/data.txt'
-graph_filename <- './dataset/cora_modified/graph.txt'
-group_filename <- './dataset/cora_modified/group.txt'
+data_filename <- '../dataset/cora_modified/data.txt'
+graph_filename <- '../dataset/cora_modified/graph.txt'
+group_filename <- '../dataset/cora_modified/group.txt'
 
-data.text <- readLines(data_filename, encoding = "utf8")
-data.graph <- data.frame(read.table(graph_filename))
-colnames(data.graph) <- c('tar', 'src')
 data.group <- readLines(group_filename)
-
-# Extraction des vecteurs de Documents Embedding
-N <- 50 # size embedding
-nobs <- length(data.text)
-d <- matrix(runif(nobs * N), ncol = N)
-
-# data.group <- data.frame( round(runif(nobs, min = 0, max = 6)) )
 data.group <- as.matrix(data.group)
 data.group <- factor(data.group)
+empty <- which(data.group == '')
+data.group <- data.group[-empty]
+
+data.text <- readLines(data_filename, encoding = "utf8")
+data.text <- data.text[-empty]
+data.docsplit <- strsplit(data.text, ' ')
+
+data.graph <- data.frame(read.table(graph_filename))
+colnames(data.graph) <- c('tar', 'src')
 
 
+#### Extraction des vecteurs de documents
+
+source('baseline.R')
+
+# Calcul des pondérations tfidf et okapi
+tfidf_avg <- weight_embedding(corpus = data.text, ponderation = 'tfidf')
+okapi_avg <- weight_embedding(corpus = data.text, ponderation = "okapi")
+
+
+# load word embedding vectors
+file_vec <- "../dataset/wiki-news-300d-1M.vec/wiki-news-300d-1M.vec"
+word_embedding <- load_word_embedding_vector(file_vec)
+
+
+doc_emb_bar <- sapply( data.docsplit, function(doc) document_embedding(word_embedding, doc, methode = "barycentre") )
+doc_emb_tfidf <- sapply( data.docsplit, function(doc) document_embedding(word_embedding, doc, methode = "tfidf", weights = tfidf_avg) )
+doc_emb_okapi <- sapply( data.docsplit, function(doc) document_embedding(word_embedding, doc, methode = "okapi", weights = okapi_avg) )
+
+
+
+
+
+
+#### Tâche d'évaluation: Classification avec plusieurs méthodes de régression
+# Création des données d'entrainement et de test
+nobs <- length(data.group)
+n_sample_train <- round(0.8 * nobs)
+n_sample_test <- nobs - n_sample_train
+set.seed(123)
+training_ind <- sample(nobs, n_sample_train)
+testing_ind <- setdiff(1:nobs, training_ind)
+
+prf <- function(truth, pred){
+  cont_tab <- table(truth, pred)
+  precision <- mean( diag(cont_tab) / colSums(cont_tab), na.rm = TRUE ) 
+  recall <- mean( diag(cont_tab) / rowSums(cont_tab), na.rm = TRUE ) 
+  f1_score <- 2 * (precision * recall) / (precision + recall) 
+  return( list(precision = precision, recall = recall, f1_score = f1_score, tab_err = cont_tab))
+}
+
+
+
+### Entrainement et test de modèles avec régression logistique et SVM
 library(MASS)
-logistic <- polr(data.group ~ d, method = "logistic")
+library(e1071)
+
+
+
+## Vecteurs barycentriques ##
+training_data <- doc_emb_bar[, training_ind]
+testing_data <- doc_emb_bar[, testing_ind]
+training_labels <- data.group[training_ind]
+testing_labels <- data.group[testing_ind]
+
+train <- data.frame(x = t(training_data), y = training_labels)
+test <- data.frame(x = t(testing_data), y = testing_labels)
+
+# Régression logistique
+logistic_bar <- polr(y ~ ., data = train, method = "logistic")
+pred_logistic_bar <- predict(logistic_bar, test )
+prf_logistic_bar <- prf(test$y, pred_logistic_bar)
+
+# SVM
+svm_model_bar = svm(y ~ ., data = train )
+pred_svm_bar <- predict(svm_model_bar, t(testing_data) )
+prf_svm_bar <- prf(test$y, pred_svm_bar)
+
+
+
+
+## Vecteurs tf-idf ##
+training_data <- doc_emb_tfidf[, training_ind]
+testing_data <- doc_emb_tfidf[, testing_ind]
+
+train <- data.frame(x = t(training_data), y = training_labels)
+test <- data.frame(x = t(testing_data), y = testing_labels)
+
+# Régression logistique
+logistic_tfidf <- polr(y ~ ., data = train, method = "logistic")
+pred_logistic_tfidf <- predict(logistic_tfidf, test )
+prf_logistic_tfidf <- prf(test$y, pred_logistic_tfidf)
+
+# SVM
+svm_model_tfidf = svm(y ~ ., data = train )
+pred_svm_tfidf <- predict(svm_model_tfidf, t(testing_data) )
+prf_svm_tfidf <- prf(test$y, pred_svm_tfidf)
+
+
+
+
+## Vecteurs okapi ##
+training_data <- doc_emb_okapi[, training_ind]
+testing_data <- doc_emb_okapi[, testing_ind]
+
+train <- data.frame(x = t(training_data), y = training_labels)
+test <- data.frame(x = t(testing_data), y = testing_labels)
+
+# Régression logistique
+logistic_okapi <- polr(y ~ ., data = train, method = "logistic")
+pred_logistic_okapi <- predict(logistic_okapi, test )
+prf_logistic_okapi <- prf(test$y, pred_logistic_okapi)
+
+# SVM
+svm_model_okapi = svm(y ~ ., data = train )
+pred_svm_okapi <- predict(svm_model_okapi, t(testing_data) )
+prf_svm_okapi <- prf(test$y, pred_svm_okapi)
 
 
 
