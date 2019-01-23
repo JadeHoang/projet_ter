02191e9eaf6af9981104e79721a6efac7eda826e
Pierre Prablanc <pierre.prablanc.pro@gmail.com>
2019-01-23 13:59:18 +0100

    Split fonctions dans evaluation_functions.R + remplacement régression logistique par naive bayes

--- src/evaluation.R
+++ src/evaluation.R
@@ -26,12 +26,15 @@ group_filename <- '../dataset/cora_modified/group.txt'
 data.group <- readLines(group_filename)
 data.group <- as.matrix(data.group)
 data.group <- factor(data.group)
-empty <- which(data.group == '')
-data.group <- data.group[-empty]
+empty <- which(data.group == '') # On relève les indices des cases vides
+data.group <- data.group[-empty] # On enlève les données vides
 
 data.text <- readLines(data_filename, encoding = "utf8")
-data.text <- data.text[-empty]
-data.docsplit <- strsplit(tolower(data.text), ' ')
+# On enlève les données documents correspondant aux groupes non renseignés
+data.text <- data.text[-empty] 
+# On créé une liste de documents dont chaque élément est une liste de mots. Les
+# caractères son mis en minuscule.
+data.docsplit <- strsplit(tolower(data.text), ' ') 
 
 data.graph <- data.frame(read.table(graph_filename))
 colnames(data.graph) <- c('tar', 'src')
@@ -50,7 +53,7 @@ okapi_avg <- weight_embedding(corpus = data.text, ponderation = "okapi")
 file_vec <- "../dataset/wiki-news-300d-1M.vec/wiki-news-300d-1M.vec"
 # file_vec <- "../dataset/Common_Crawl_Wikipedia/cc.en.300.vec/data"
 # file_vec <- "../dataset/GoogleNews-vectors-negative300.bin/data"
-word_embedding <- load_word_embedding_vector("../dataset/wiki-news-300d-1M.vec/wiki-news-300d-1M.vec", nrows = 120000)
+word_embedding <- load_word_embedding_vector("../dataset/wiki-news-300d-1M.vec/wiki-news-300d-1M.vec", nrows = 50000)
 we <- read.csv("../dataset/GoogleNews-vectors-negative300.bin/data", 
                  header = FALSE, 
                  stringsAsFactors = FALSE, 
@@ -69,54 +72,20 @@ doc_emb_okapi <- sapply( data.docsplit, function(doc) document_embedding(word_em
 
 #### Tâche d'évaluation: Classification avec 2 méthodes de régression (Bayésien Naïf / SVM)
 # Création des données d'entrainement et de test
-generate_train_test <- function(x, y, training_amount){
-  nobs <- length(y)
-  n_sample_train <- round(training_amount * nobs)
-  n_sample_test <- nobs - n_sample_train
-  training_ind <- sample(nobs, n_sample_train)
-  test_ind <- setdiff(1:nobs, training_ind)
-  
-  training_x <- x[, training_ind]
-  test_x <- x[, test_ind]
-  training_y <- y[training_ind]
-  test_y <- y[test_ind]
-  
-  train <- data.frame(x = t(training_x), y = training_y)
-  test <- data.frame(x = t(test_x), y = test_y)
-  
-  return( list(train = train, test = test))
-}
-
-
-prf <- function(truth, pred){
-  cont_tab <- table(truth, pred)
-  precision <- mean( diag(cont_tab) / colSums(cont_tab), na.rm = TRUE ) 
-  recall <- mean( diag(cont_tab) / rowSums(cont_tab), na.rm = TRUE ) 
-  f1_score <- 2 * (precision * recall) / (precision + recall) 
-  return( list(precision = precision, recall = recall, f1_score = f1_score, tab_err = cont_tab))
-}
-
-mean_prf <- function(x){
-  precision_ind <- which(names(x) == "precision")
-  recall_ind <- which(names(x) == "recall")
-  f1_score_ind <- which(names(x) == "f1_score")
-  mean_precision <- mean(x[precision_ind]$precision)
-  mean_recall <- mean(x[recall_ind]$recall)
-  mean_f1_score <- mean(x[f1_score_ind]$f1_score)
-  return( list( precision = mean_precision, recall = mean_recall, f1_score = mean_f1_score))
-}
 
 
 ### Entrainement et test de modèles avec Bayésien Naïf et SVM
 library(MASS)
 library(e1071)
+source('evaluation_functions.R')
 
+ta <- 0.1 # Pourcentage de données d'entrainement
 
 prf_nb_bar <- c()
 prf_svm_bar <- c()
 for(i in 1:10){
   ## Vecteurs barycentriques ##
-  dataset <- generate_train_test(doc_emb_bar, data.group, 0.1)
+  dataset <- generate_train_test(doc_emb_bar, data.group, training_amount = ta)
   
   # Bayésien Naïf
   nb_bar <- naiveBayes(y ~ ., data = dataset$train)
@@ -137,7 +106,7 @@ prf_nb_tfidf <- c()
 prf_svm_tfidf <- c()
 for(i in 1:10){
   ## Vecteurs tf-idf ##
-  dataset <- generate_train_test(doc_emb_tfidf, data.group, training_amount = 0.1)
+  dataset <- generate_train_test(doc_emb_tfidf, data.group, training_amount = ta)
   
   # Bayésien Naïf
   nb_tfidf <- naiveBayes(y ~ ., data = dataset$train)
@@ -157,7 +126,7 @@ prf_nb_okapi <- c()
 prf_svm_okapi <- c()
 for(i in 1:10){
   ## Vecteurs okapi ##
-  dataset <- generate_train_test(doc_emb_okapi, data.group, training_amount = 0.1)
+  dataset <- generate_train_test(doc_emb_okapi, data.group, training_amount = ta)
   
   # Bayésien Naïf
   nb_okapi <- naiveBayes(y ~ ., data = dataset$train)
